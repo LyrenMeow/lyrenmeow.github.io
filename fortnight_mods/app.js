cb328537248cea1c657ca35c90d71df03e2e608c
Lyren <lyrenmeow@gmail.com>
2024-03-15 16:27:23 +0200

    Fortnight Mods #4 update
    Removed WR, updated title, updated timer (deadline at start of 29th of March) - The Thing Remaster

--- fortnight_mods/app.js
+++ fortnight_mods/app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("March 15, 2024 00:00:00").getTime();
+    const countDate = new Date("March 29, 2024 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
