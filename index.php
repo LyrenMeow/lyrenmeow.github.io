3465c05f391526e1f6ea3f5f960b7514815046f9
Lyren <vainomakinen@gmail.com>
2024-11-07 02:02:29 +0200

    Added main page (affension)

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Affension</title>
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="style.css">
+
+<body>
+
+  <div style="background-color:rgb(34,34,34);">
+  <img src="affension-logo.png" style="display: block;margin-left: auto;margin-right: auto;width: 18%;">
+</div>
+
+<div>
+  <table style="margin-left: auto;margin-right: auto;">
+    <tr>
+      <td class="top-bar"><a href="#main">MAIN</a></td>
+      <td class="top-bar"><a href="#side">SIDE</a></td>
+      <td class="top-bar"><a href="#recomms">RECOMMS</a></td>
+      <td class="top-bar" style="border-right:solid 2px rgb(63,63,63)"><a href="#tutorials">TUTORIALS</a></td>
+    </tr>
+  </table>
+</div>
+
+<div style="background: url('Pink-75.png'), linear-gradient(rgb(40,40,40) 0%, rgb(31,31,31) 35%);background-repeat: no-repeat;background-position: right bottom;height:38rem;">
+  <h1>MEET THE TEAM</h1>
+
+  <div class="boxes">
+    <div class="box" style="margin-left:8rem;margin-top:1rem;">
+      <h2>PROJECT LEAD</h2>
+      <hr>
+      <table>
+        <tr>
+          <td><a href="https://lyren.dazonia.xyz" title=""><img src="Lyren.jpg" style="width:10rem;border:1px solid #253562;padding:0px;"></td>
+          <td style="width:0rem;padding:0.7rem;"></td>
+          <td><a href="https://martiin.lima-city.de" title=""><img src="martin.png" style="width:10rem;border:1px solid #253562;padding:0px;"></td>
+        </tr>
+        <tr>
+          <td style="padding:0px;">Lyren</td>
+          <td style="width:0rem;"></td>
+          <td style="padding:0px;">martin</td>
+        </tr>
+        <tr>
+          <td style="padding:0px;font-size:1.4rem;">Designer</td>
+          <td style="width:0rem;"></td>
+          <td style="padding:0px;font-size:1.4rem;">Programmer</td>
+        </tr>
+      </table>
+    </div class="box">
+    <div class="box" style="margin-left:4rem;margin-top:1rem;">
+      <h2 style="margin-left:1rem;">XEN DEVELOPMENT TEAM</h2>
+<!--      <table>
+        <tr>
+          <td style="border:1px solid #253562;padding:0px;"><img src="Lyren.png" style="width:8rem;"></td>
+          <td style="width:0rem;padding:0.7rem;"></td>
+          <td style="border:1px solid #253562;padding:0px;"><img src="martin.png" style="width:8rem;"></td>
+          <td style="width:0rem;padding:0.7rem;"></td>
+          <td style="border:1px solid #253562;padding:0px;"><img src="Lyren.png" style="width:8rem;"></td>
+          <td style="width:0rem;padding:0.7rem;"></td>
+          <td style="border:1px solid #253562;padding:0px;"><img src="martin.png" style="width:8rem;"></td>
+        </tr>
+      </table> -->
+<small style="margin-left:10rem;"><a href="xendev">List of Xen Dev projects</a></small>
+
+      </div class="box">
+      <h2 style="margin-left:40rem;margin-top:-3rem;">CONTRIBUTORS</h2>
+    <div class="box" style="margin-left:4rem;margin-top:1rem;">
+      <table style="margin-left:-4rem;margin-top:-1rem;">
+             <tr>
+               <td><a href="https://steamcommunity.com/id/Neaty/" title="Web host 2020-"><img src="Kar.png" style="width:5rem;border:1px solid #253562;padding:0px;"></td>
+               <td style="width:0rem;padding:0.5rem;"></td>
+               <td><img src="empty.png" style="width:5rem;border:1px solid #253562;padding:0px;"></td>
+               <td style="width:0rem;padding:0.5rem;"></td>
+               <td><img src="empty.png" style="width:5rem;border:1px solid #253562;padding:0px;"></td>
+               <td style="width:0rem;padding:0.5rem;"></td>
+               <td><img src="empty.png" style="width:5rem;border:1px solid #253562;padding:0px;"></td>
+               <td style="width:0rem;padding:0.5rem;"></td>
+               <td><img src="empty.png" style="width:5rem;border:1px solid #253562;padding:0px;"></td>
+               <td style="width:0rem;padding:0.5rem;"></td>
+               <td><img src="empty.png" style="width:5rem;border:1px solid #253562;padding:0px;"></td>
+             </tr>
+             <tr>
+               <td style="padding:0px;font-size:1.4rem;"><a href="https://steamcommunity.com/id/Neaty/" title="Web host 2020-">Kar</a></td>
+               <td style="width:0rem;"></td>
+             </tr>
+           </table>
+      </div class="box">
+        </div class="boxes">
+</div>
+
+<a name="main"/></a>
+<div style="background: url('Lyz-65.png'), linear-gradient(rgb(40,40,40) 0%, rgb(31,31,31) 25%);background-repeat: no-repeat;background-position: left bottom;height:46rem;">
+  <h3>MAIN PROJECTS</h3>
+  <table class="project-table">
+         <tr>
+          <td class="project" style="color:#4fb4e6;background:url('Dazonia-smol.png');background-position:left top;background-repeat:no-repeat;"><a href="https://dazonia.xyz">DAZONIA</td>
+          <td style="width:0rem;"></td>
+          <td class="project" style="color:#50b045;background:url('Garden-smol.png');background-position:left top;background-repeat:no-repeat;"><a href="https://affension.dazonia.xyz/releases/Garden.7z" download>GARDEN</a></td>
+         </tr>
+         <tr>
+           <td style="font-size:1.5rem;max-width:17rem;">Image board with various content</td>
+           <td style="width:14rem;"></td>
+           <td style="font-size:1.5rem;max-width:17rem;">Sorting / browsing local images & files</td>
+         </tr>
+         <tr>
+          <td class="project" style="color:#80b685;font-size:2.2rem;">DOUBLER</td>
+          <td style="width:0rem;"></td>
+          <td class="project" style="color:#59c5bc;font-size:2.2rem;">COZYDAYS</td>
+         </tr>
+         <tr>
+           <td style="font-size:1.5rem;max-width:17rem;">Create duplicate files through your explorer context menu</td>
+           <td style="width:14rem;"></td>
+           <td style="font-size:1.5rem;max-width:17rem;">Anime-styled slife of life web comic made by Lyren</td>
+         </tr>
+         <tr>
+          <td class="project" style="color:#b0b0b0;font-size:2.4rem;">SWOD</td>
+         </tr>
+         <tr>
+           <td style="font-size:1.5rem;max-width:17rem;">Download songs & videos with a single click</td>
+         </tr>
+       </table>
+</div>
+
+<a name="side"/></a>
+<div style="background: url('Fuyu-60.png'), linear-gradient(rgb(40,40,40) 0%, rgb(31,31,31) 20%);background-repeat: no-repeat;background-position: right bottom;height:46rem;">
+  <h3 style="text-align:right;">SIDE PROJECTS</h3>
+  <table class="project-table" style="margin-left:5%;margin-top:0px;">
+         <tr>
+          <td class="project2" style="font-size:1.6rem;background:url('hopploader-smol.png');background-position:left top;background-repeat:no-repeat;"><a href="https://chromewebstore.google.com/detail/hopploader/lgcnfaldckhibfkdckhkboobbjcejmjb?pli=1" download>HOPPLOADER</a></td>
+          <td style="width:0rem;"></td>
+          <td class="project2" style="font-size:1.4rem;">DISCORD THEME</td>
+          <td style="width:0rem;"></td>
+          <td class="project2" style="font-size:1.6rem;">FOOBAR THEME</td>
+         </tr>
+         <tr>
+           <td style="font-size:1.2rem;max-width:12rem;">Downloads an image from an open tab and auto-closes it</td>
+           <td style="width:5rem;"></td>
+           <td style="font-size:1.2rem;max-width:12rem;">Affension-styled theme for BetterDiscord</td>
+           <td style="width:5rem;"></td>
+           <td style="font-size:1.2rem;max-width:12rem;">Lyren's purple theme & components for the Foobar2000 music player</td>
+         </tr>
+         <tr>
+          <td class="project2" style="font-size:1.6rem;">RAINMETER</td>
+          <td style="width:0rem;"></td>
+          <td class="project2" style="font-size:1.6rem;"><a href="https://affension.dazonia.xyz/releases/video-crop.7z" download>VIDEO-CROP.BAT</a></td>
+          <td style="width:0rem;"></td>
+         </tr>
+         <tr>
+           <td style="font-size:1.2rem;max-width:12rem;">Collection of Lyren's self-made Rainmeter skins</td>
+           <td style="width:5rem;"></td>'
+           <td style="font-size:1.2rem;max-width:12rem;">Crops a video instantly with just a simple drag & drop (as well as another .bat to compress/resize videos)</td>
+           <td style="width:5rem;"></td>'
+         </tr>
+
+       </table>
+</div>
+
+<a name="recomms"/></a>
+<div style="background: url('Jenna-65.png'), linear-gradient(rgb(40,40,40) 0%, rgb(31,31,31) 20%);background-repeat: no-repeat;background-position: left bottom;height:44rem;">
+  <h3>RECOMMENDATIONS</h3>
+</div>
+
+<a name="tutorials"/></a>
+<div style="background: url(''), linear-gradient(rgb(40,40,40) 0%, rgb(31,31,31) 20%);background-repeat: no-repeat;background-position: right bottom;height:44rem;">
+  <h3 style="text-align:right;">TUTORIALS</h3>
+</div>
+<br><br>
+
+</body>
+
+</html>
